8278a8ca44bd43a2f470463f9f4801a4ecb47734
amamanic204 <amamani204@umsa.bo>
2025-09-15 07:27:31 +0900

    avance hasta lo avanzado por el auxi

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import FileResponse, JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+	return FileResponse("holaEspanol.html")
+
+@app.get("/ingles")
+async def frances():
+	return FileResponse("holaIngles.html")
+
+@app.get("/frances")
+async def frances():
+	return FileResponse("holaFrances.html")
+
+@app.get("/italiano")
+async def frances():
+	return FileResponse("holaItaliano.html")
+
+
+#coockies
+@app.get("/getFuente")
+async def getFuente(request: Request):
+	fue = request.cookies.get("fontFamily")
+	return JSONResponse({"fontFamily": fue})
+
+@app.post("/setFuente/{fuente}")
+async def setFuente(fuente: str, response: Response):
+	response = JSONResponse({"mensaje": "fuente guardada"})
+	response.set_cookie(key="fontFamily", value=fuente, max_age=60*60);
+	return response
